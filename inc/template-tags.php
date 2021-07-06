b4b3caa9c1db90551e60b4fd42edd9b5ecccf379
Tiff Lee <79429547+tiffanyleedesign@users.noreply.github.com>
2021-07-06 13:43:13 -0700

    update single news again

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -64,7 +64,8 @@ if ( ! function_exists( 'velou_entry_footer' ) ) :
 			$categories_list = get_the_category_list( esc_html__( ', ', 'velou' ) );
 			if ( $categories_list ) {
 				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'velou' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				// printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'velou' ) . '</span>', $categories_list ); 
+				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			}
 
 			/* translators: used between list items, there is a space after the comma */
