03026d65ef94a23572e10564c6d29dbb9c6d7fcf
ElenaDianaUngureanu <81187070+ElenaDianaUngureanu@users.noreply.github.com>
2021-07-11 16:00:15 -0700

    add about page tablet and desktop size style

--- page-about-velou.php
+++ page-about-velou.php
@@ -25,20 +25,21 @@ get_header();
 					</div>
 					<div class="page-hero">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 331.99 161.87"><defs><style>.cls-1{isolation:isolate;}.cls-2{fill:#4a4745;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g class="cls-1"><path class="cls-2" d="M0,.87H10.7L33.12,135.18,55.54.87h4.84L33.63,161H26.75Z"/><path class="cls-2" d="M76.69.87h45.1V4.56H87.9V76.81h29.81V80.5H87.9v76.81h35.16V161H76.69Z"/><path class="cls-2" d="M139.88.87h11.21V157.31h31.34V161H139.88Z"/><path class="cls-2" d="M188.54,80.93C188.54,15.41,200.77,0,221.92,0c21.4,0,33.63,15.41,33.63,80.93s-12.23,80.94-33.63,80.94C200.77,161.87,188.54,146.46,188.54,80.93Zm55.55,0c0-64.87-5.35-77-22.17-77-16.56,0-21.91,12.15-21.91,77s5.35,77,21.91,77C238.74,158,244.09,145.81,244.09,80.93Z"/><path class="cls-2" d="M275.68,128V.87h11.21V129.1c0,23.22,6.11,27.13,19.62,27.13s19.62-3.91,19.62-27.13V.87H332V129.1c0,26.47-9.43,33-28.28,32.33C285.36,160.78,275.68,154.92,275.68,128Z"/></g></g></g></svg>
+						<section class="mission">
+							<?php 
+							if(function_exists('get_field')){
+								if(get_field('company_mission')){?>
+									<p><?php the_field('company_mission'); ?> </p>
+								<?php
+								}
+								
+							}?>
+						</section>					
 					</div>
 				</section>
 	
 
-			<section class="mission">
-				<?php 
-				if(function_exists('get_field')){
-					if(get_field('company_mission')){?>
-						<p><?php the_field('company_mission'); ?> </p>
-					<?php
-					}
-					
-				}?>
-			</section>
+			
 
 			<!-- Stylists Section -->
 			<?php 
@@ -52,26 +53,26 @@ get_header();
 
 			if ( $query->have_posts() ){ ?>
 			<section class="stylists-section">
-				<h2>Our Award Winning Team</h2>	
-					<section class="stylists"><?php
+				<h2>Our Award Winning Team</h2>	<?php	
 
-					if(get_field('stylist_section_overview')){?>					
-						<p><?php the_field('stylist_section_overview'); ?> </p>
+				if(get_field('stylist_section_overview')){?>				
+					<p><?php the_field('stylist_section_overview'); ?> </p>
 					<?php
-					}
-						 while ( $query->have_posts() ) : $query->the_post(); ?>				
-							<div class="one-stylist">
-								<div class="image">
-									<?php the_post_thumbnail('medium'); ?>
-								</div>	
-								<h3> <?php the_title(); ?></h3>
-								<p><?php echo get_the_term_list( $post->ID, 'velou-stylist-speciality', '', ', ', ' stylist' ); ?></p>
-								<a href="<?php the_permalink(); ?>">View Profile</a>									
-							</div>				
-							<?php 
-				
-						endwhile; ?>							
-					</section>
+				}?>
+				<article class="stylists"><?php
+						while ( $query->have_posts() ) : $query->the_post(); ?>				
+						<div class="one-stylist">
+							<div class="image">
+								<?php the_post_thumbnail('medium'); ?>
+							</div>	
+							<h3> <?php the_title(); ?></h3>
+							<p><?php echo get_the_term_list( $post->ID, 'velou-stylist-speciality', '', ', ', ' stylist' ); ?></p>
+							<a href="<?php the_permalink(); ?>">View Profile</a>									
+						</div>				
+						<?php 
+			
+					endwhile; ?>							
+				</article>
 			</section>
 			<?php
 			wp_reset_postdata();
@@ -146,17 +147,17 @@ get_header();
 
 			<!-- CTA Section -->
 			<section class="cta">	
-
-				<!-- Contact Us CTA -->
-				<div class="cta-solid cta-contact">
-					<a href="<?php echo get_permalink(37); ?>">Contact Us</a>
+				<div class="inner-cta">
+					<!-- Contact Us CTA -->
+					<div class="cta-solid cta-contact">
+						<a href="<?php echo get_permalink(37); ?>">Contact Us</a>
+					</div>
+					<!-- FAQ CTA -->
+					<div class="cta-hollow cta-faq">
+						<a href="<?php echo get_permalink(12); ?>">FAQ</a>
+					</div>
 				</div>
-
-				<!-- FAQ CTA -->
-				<div class="cta-hollow cta-faq">
-					<a href="<?php echo get_permalink(12); ?>">FAQ</a>
-				</div>
-				</section>
+			</section>
 		<?php endwhile; // End of the loop.?>
 	</main><!-- #main -->
 <?php
